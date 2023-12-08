e30986a30af3cd1a0b32073475f7129c229d5b26
Anna Pecheanu <annapecheanu1@gmail.com>
2023-12-08 21:12:00 +0200

    Created a simple, parrot-like program

---
+++ parrot_service/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <syslog.h>
+
+int main(int argc, char *argv[]) {
+   
+/*     if (argc != 2) {
+        fprintf(stderr, "Usage: %s <message> %d\n", argv[0], argc);
+        return 1;
+    } */
+    printf("%d %s %s\n", argc, argv[0], argv[1]);  
+    const char *message = argv[1];
+    FILE *log_file;
+    log_file = fopen("log.txt", "w");
+    if (log_file == NULL) {
+	perror("error open file");
+	return 1;
+    }
+    for (int i = 0; i < 10; ++i) {
+	fprintf(log_file, "%s\n", message);
+	fflush(log_file);
+	sleep(2);
+    }
+    fclose(log_file);
+   // int i = 0;
+    //while (1) {
+    //    printf("%s %d\n", message, i);
+     //   ++i;
+       // sleep(1);
+   // }
+   return 0;
+}
